53e977d2b018c85ace2b3cdcdd094aead92909b6
errasti <errasti007@yandex.ru>
2022-08-14 23:36:14 +0300

    Initial commit

---
+++ task_02/Program.cs
@@ -0,0 +1,34 @@
+﻿int[,] CreateArray(int row, int cow)
+{
+    int[,] array = new int[row, cow];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for ( int i = 0; i < array.GetLength(0); i++)
+    {
+        for ( int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк и столбцов через пробел");
+string[] input = Console.ReadLine().Split();
+int row = int.Parse(input[0]);
+int col = int.Parse(input[1]);
+CreateArray(row,col);
+int[,] numbers = CreateArray(row,col);
+PrintArray(numbers);
+
