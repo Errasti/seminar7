7c81db9b45beeb31df87d410bee124f510086ff1
errasti <errasti007@yandex.ru>
2022-08-17 10:12:39 +0300

    Initial commit

---
+++ hw_01/Program.cs
@@ -0,0 +1,33 @@
+﻿double[,] CreateArray(int row, int col)
+{
+    double[,] array = new double[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(rnd.Next(-1000, 999) / 10.0);
+        }
+    }
+    return array;
+}
+
+void ShowArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк и столбцов через пробел: ");
+string[] input = Console.ReadLine().Split();
+int row = int.Parse(input[0]);
+int col = int.Parse(input[1]);
+double[,] myArray = CreateArray(row, col);
+ShowArray(myArray);
