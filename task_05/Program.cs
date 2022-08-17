17d6d6b087499bd98ad29bd637a0984cf1666de8
errasti <errasti007@yandex.ru>
2022-08-17 09:39:38 +0300

    Initial commit task5

---
+++ task_05/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int Factorial(int number)
+{
+    if (number == 1)
+    {
+        return number;
+    }
+
+    return number * Factorial(number - 1);
+}
+
+
+Console.WriteLine(Factorial(num));
